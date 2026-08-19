43f27c0d82f2f6dba9a1b6a95be95e23ea80e06b
Ha-Jun Lee <hajune1017gaming@gmail.com>
2026-08-19 16:52:09 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/8daudiomaker/', // 저장소 이름과 정확히 일치, 앞뒤 슬래시 필수
 })
